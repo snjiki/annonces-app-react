279c1560da68fa57e6cb4443eec225bf8bf31458
Stéphane NJIKI <stephane.njiki@gmail.com>
2019-01-13 10:00:34 +0100

    API URL Ok

--- src/api.js
+++ src/api.js
@@ -3,9 +3,9 @@ import axios from "axios";
 export default {
   user: {
     login: (credentials) =>
-      axios.post("/api/auth", { credentials }).then(res => res.data.user),
+      axios.post("localhost:8000/api/auth", { credentials }).then(res => res.data.user),
 
     signin: (credentials) =>
-      axios.post("/api/auth", { credentials }).then(res => res.data.user)
+      axios.post("localhost:8000/api/user", { credentials }).then(res => res.data.user)
   }
 };
